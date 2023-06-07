08e17075718fe2e6b54168972c179c7ff263857f
Yusuf Olosan <depolymathyousouph@gmail.com>
2023-06-07 07:36:30 +0100

    Day2 of Pelpay App

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,12 +1,96 @@
 import classes from "./About.module.css";
 import hero from "../assets/Browser.png";
+import img1 from "../assets/about-img.png";
+import img2 from "../assets/about-img2.png";
+import supportive from "../assets/supportive.svg";
+import innovative from "../assets/innovative.svg";
+import authenticity from "../assets/authenticity.svg";
+import ownership from "../assets/ownership.svg";
 const About = () => {
   return (
     <>
       <div className={classes.hero}>
-        <h1>Enabling the New Wave of Africa Commerce</h1>
+        <div className={classes.text}>
+          <h1>
+            Enabling the New Wave
+            <br /> of Africa Commerce
+          </h1>
+          <p>
+            Pelpay builds technology to help Africa’s best businesses grow -
+            from new startups, to market leaders launching new business models.
+          </p>
+        </div>
         <img src={hero} alt="hero-image" />
       </div>
+      <section className={classes.section}>
+        <h1 className={classes.h1}>
+          Copy Connecting <span>African Commerce</span> to the Rest of the World
+        </h1>
+        <p>
+          Pelpay’s vision is to make it seamless for Africans to build
+          businesses that can transact with anyone across the world
+        </p>
+        <div className={classes.img}>
+          <img src={img1} alt="A man explain something to lady" />
+          <img src={img2} alt="A man explain something to lady" />
+        </div>
+      </section>
+      <article className={classes.article}>
+        <ul className={classes.list}>
+          <li>
+            <img src={ownership} alt="ownership icon" />
+            <h4>Ownership</h4>
+            <p>
+              Ownership underpin our spirit of ownership, and commitment to
+              drive mission forward.
+            </p>
+          </li>
+          <li>
+            <img src={supportive} alt="supportive icon" />
+            <h4>Supportive</h4>
+            <p>
+              We don’t stop until we nail it. Then we keep going. We stand
+              together to stand tall.
+            </p>
+          </li>
+          <li>
+            <img src={innovative} alt="innovative icon" />
+            <h4>Innovative</h4>
+            <p>
+              Creating a future-proof digital payment platform for Africa takes
+              a trailblazing team —
+            </p>
+          </li>
+
+          <li>
+            <img src={authenticity} alt="authenticity icon" />
+            <h1>Authenticity</h1>
+            <p>
+              We believe that every person matters and we all take a transparent
+              view on the status quo.
+            </p>
+          </li>
+        </ul>
+        <aside>
+          <h2>Payment. We get it Absolutly!</h2>
+          <p className={classes.paragraph}>
+            The dedication to our products shows. That’s why we’re so stoked to
+            support you with exactly the kind of payment solutions you’re
+            looking for.
+          </p>
+          <div className={classes.flex}>
+            <div>
+              <h1>300M+</h1>
+              <p>Transactions every day</p>
+            </div>
+            <div>
+              <h1>1,000+</h1>
+              <p>African businesses around the world</p>
+            </div>
+          </div>
+          <button>View Documentation</button>
+        </aside>
+      </article>
     </>
   );
 };
--- src/pages/Developers.tsx
+++ src/pages/Developers.tsx
@@ -1,4 +1,91 @@
+import hero from "../assets/Image with shapes.svg";
+import classes from "./Developers.module.css";
+import analytics from "../assets/Analytics Graphic.svg";
+import service_img from "../assets/Service03.png";
+import image from "../assets/image.png";
+import secure_image from "../assets/secured payment.png";
 const Developers = () => {
-  return <>Developer</>;
+  return (
+    <>
+      <div className={classes.hero}>
+        <div>
+          <h1>
+            The <span>API suite</span> to build amazing payments products
+          </h1>
+          <p>
+            Copy Power up your payments with comprehensive APIs that help you
+            create sophisticated payment flows and gain operational
+            efficiencies.
+          </p>
+          <button className={classes.button}>View Documentation</button>
+        </div>
+        <img src={hero} alt="hero" />
+      </div>
+      <section className={classes.section}>
+        <img src={analytics} alt="Analytics" />
+        <aside>
+          <span>
+            <div className={classes.line}></div>Develop
+          </span>
+          <h1>Experience how our platform empowers developers</h1>
+          <p>
+            Spin up your own private sandbox and start developing in minutes.
+            Explore our endpoints, make API requests, and quickly prototype your
+            product.
+          </p>
+          <button className={classes.button}>View Documentation</button>
+        </aside>
+      </section>
+      <section className={classes.section}>
+        <div>
+          <h1>More flexibility, less complexity</h1>
+          <p>
+            Your customers’ payment experience is fully automated so payments
+            take place with minimal effort. Monitor transaction insights such as
+            sales, customer information, success rates, and more!
+          </p>
+          <ul className={classes.list}>
+            {[
+              "Get alerts for important updates",
+              "Setup payment links and Look up transactions",
+              "Start with done-for-you account setup",
+            ].map((val, index) => (
+              <li key={index}>{val}</li>
+            ))}
+          </ul>
+        </div>
+        <img src={service_img} alt="service image" />
+      </section>
+      <section className={classes.section}>
+        <img src={image} alt="Created payment flows" />
+        <div>
+          <h1>Create automated payment flows</h1>
+          <p>
+            Copy Churn Buster takes the right action at the right times to
+            preserve customer relationships, elevate your brand, and retain more
+            customers long-term.
+          </p>
+        </div>
+      </section>
+      <section className={classes.section}>
+        <div>
+          <h1>Fraud Protection and Transaction Monitoring</h1>
+          <p>
+            PelPay combination of automated and manual fraud systems protect you
+            from fraudulent transactions and associated chargeback claims.
+          </p>
+          <ul className={classes.list}>
+            {[
+              "Get alerts for important updates",
+              "Setup payment links and Look up transactions",
+            ].map((val, index) => (
+              <li key={index}>{val}</li>
+            ))}
+          </ul>
+        </div>
+        <img src={secure_image} alt="secure_image" />
+      </section>
+    </>
+  );
 };
 export default Developers;
